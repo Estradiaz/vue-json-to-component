d31ec62db38e0fadbae58690e5abbf552569a8d5
Samuel Ueberle <s.ueberle@fu-berlin.de>
2019-06-14 16:24:13 +0200

    fix: uses precompiled

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+    <json2comp :items="items" tag="table">
+            <!-- <template #tr="{item}">
+                <tr :class="item.class">
+                    overwrites following templates in
+                    {{item.items}}
+                </tr>
+            </template> -->
+            <template #td="{item}">
+                <td :class="item.class">
+                {{item}}
+                </td>
+            </template>
+        </json2comp> 
+</template>
+
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator'
+import { AbstractElement } from './json2comp';
+@Component
+export default class extends Vue{
+    items: AbstractElement[] = [
+        {
+            ElementTag: 'thead',
+            class:"thead-class",
+            items: [
+                {
+                    ElementTag: 'tr',
+                    class:"tr-class",
+                    items: [
+                        {
+                            ElementTag: 'th',
+                            class:"th-class",
+                            innerText: 'sampleText'
+                        },
+                        {
+                            ElementTag: 'th',
+                            class:"th-class",
+                            items: [{
+
+                                ElementTag: 'h3',
+                                innerText: 'Title'
+                            }
+                            ]
+                        },
+                    ]
+                },      
+            ]
+        },
+        {
+            ElementTag: 'tbody',
+            class:"tbody-class",
+            items: [
+                {
+                    ElementTag: 'tr',
+                    class:"tr-class",
+                    items: [
+                        {
+                            ElementTag: 'td',
+                            class:"td-class",
+                            items: [
+                                {ElementTag: 'input',
+                                type: 'text',
+                                value: ""}
+                            ]
+                        },
+                        {
+                            ElementTag: 'td',
+                            class:"td-class",
+                            items: [
+                                {ElementTag: 'input',
+                                type: 'date',
+                                value: ""}
+                            ]
+                        },
+                    ]
+                },      
+            ]
+        },
+    ]
+}
+
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,8 @@
 import Vue from 'vue'
 import Json2Comp from './json2comp'
+import App from './App.vue'
 Vue.config.productionTip = false
 Vue.component('json2comp', Json2Comp);
 
-new Vue({
-    render: h => h(Json2Comp, {
-        attrs: {
-            id: 'app',
-        },
-        props: {
-            tag: 'div',
-            items: [
-                {
-                    ElementTag: 'h1',
-                    innerText: 'test',
-                    id: 'title'
-                },
-                {
-                    ElementTag: 'ul',
-                    items: [
-                        {
-                            ElementTag: 'li',
-                            innerText: 'test',
-                            items: [{
-                                ElementTag: 'ul',
-                                items: [{
-                                    ElementTag: 'li',
-                                    innerText: 'test'
-                                },      
-                                {
-                                    ElementTag: 'li',
-                                    innerText: 'test'
-                                }]
-                            }]
-                        },      
-                        {
-                            ElementTag: 'li',
-                            innerText: 'test'
-                        },      
-                        {
-                            ElementTag: 'li',
-                            innerText: 'test'
-                        },      
-                    ],
-                    innerText: 'overwrite'
-                }
-            ]
-        }
-    })
-}).$mount('#app')
+new Vue(App)
+.$mount('#app')
