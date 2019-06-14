f09b3bd04fa722bbec857b079a9d44b0e95ed614
Samuel Ueberle <s.ueberle@fu-berlin.de>
2019-06-14 14:06:18 +0200

    feature: adds option to add children by  items

--- src/json2comp.ts
+++ src/json2comp.ts
@@ -1,32 +1,56 @@
 import {Vue, Component, Prop} from 'vue-property-decorator'
+import { CreateElement, RenderContext, VNode } from 'vue';
 export type AbstractElement = {
     [key: string]: any // passed as $attrs |  useable for assigned $props
     ElementTag: string
     slotName?: string
+    innerText?: string
+    items?: AbstractElement[]
+    // listeners?: should be assigned through slots
 }
 @Component<List>({
-
+    
     render(h){
-
-        let tag = this.tag 
+        let renderItem = setRenderItemScope(this, h)
+        let tag = this.tag
         || (this.$parent.$vnode && this.$parent.$vnode.tag)
         || (this.$parent.$el && this.$parent.$el.tagName)
-        if(tag === undefined) throw Error(`tag prperty: ${tag} is invalid. Scope within valid vnode tag or pass valid component/ html tag as property`)
-        return h(tag, this.items.map(item => {
-            const {ElementTag, slotName, ...attrs} = item;
-            console.log("slotName", slotName)
-            return (this.$scopedSlots[slotName || ElementTag]
-            //@ts-ignore
-            && this.$scopedSlots[slotName || ElementTag]({item}))
-            || h(ElementTag, {
-                attrs: attrs,
-                slot: slotName || ElementTag,
-                scopedSlots: this.$scopedSlots
-            })
-        }))
+        if(tag === undefined) throw Error(
+            `tag prperty: ${tag} is invalid. Scope within element or pass valid component/ html tag as property`)
+        return h(
+            tag,
+            {
+                attrs: this.$attrs
+            },
+            this.items.map(renderItem)
+        )
     }
 })
 export default class List extends Vue{
     @Prop(String) readonly tag?: string
     @Prop(Array) readonly items!: Array<AbstractElement>
+}
+export function setRenderItemScope(ctx: List, h: CreateElement){
+    return function renderItem(item: AbstractElement): VNode | VNode[] {
+
+        const {ElementTag, slotName, innerText, items: _items, ...attrs} = item;
+        const scopedSlot = ctx.$scopedSlots[slotName || ElementTag]
+        return (
+            scopedSlot && scopedSlot({item})
+        ) || h(
+            ElementTag, 
+            {
+                attrs: attrs,
+                slot: slotName || ElementTag,
+                scopedSlots: ctx.$scopedSlots,
+                on:{}
+            }, 
+            _items && _items.map(renderItem)
+            || innerText && [
+                //@ts-ignore
+                ctx._v(innerText)
+            ] 
+            || []
+        )
+    }
 }
