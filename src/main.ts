ad849e4a91203b19ad4190aeb45ca4fc997c0776
Samuel Ueberle <s.ueberle@fu-berlin.de>
2019-06-14 14:06:58 +0200

    example: adds example

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,51 @@
 import Vue from 'vue'
-
+import Json2Comp from './json2comp'
 Vue.config.productionTip = false
+Vue.component('json2comp', Json2Comp);
 
-new Vue().$mount('#app')
+new Vue({
+    render: h => h(Json2Comp, {
+        attrs: {
+            id: 'app',
+        },
+        props: {
+            tag: 'div',
+            items: [
+                {
+                    ElementTag: 'h1',
+                    innerText: 'test',
+                    id: 'title'
+                },
+                {
+                    ElementTag: 'ul',
+                    items: [
+                        {
+                            ElementTag: 'li',
+                            innerText: 'test',
+                            items: [{
+                                ElementTag: 'ul',
+                                items: [{
+                                    ElementTag: 'li',
+                                    innerText: 'test'
+                                },      
+                                {
+                                    ElementTag: 'li',
+                                    innerText: 'test'
+                                }]
+                            }]
+                        },      
+                        {
+                            ElementTag: 'li',
+                            innerText: 'test'
+                        },      
+                        {
+                            ElementTag: 'li',
+                            innerText: 'test'
+                        },      
+                    ],
+                    innerText: 'overwrite'
+                }
+            ]
+        }
+    })
+}).$mount('#app')
